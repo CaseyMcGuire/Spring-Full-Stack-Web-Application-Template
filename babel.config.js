b716bbfc44c44b64124b78c221b5bac9eaf467d3
CaseyMcGuire <caseyjaymcguire@gmail.com>
2026-05-25 21:30:53 -0700

    Add stylex sx prop support
    Augments React's DOMAttributes interface so the babel-plugin's built-in
    sx prop transform can be used directly on HTML elements, mirroring
    caseymcguire.com.
    
    Drops @stylexjs/babel-plugin from babel.config.js and package.json
    since @stylexjs/unplugin (already in webpack) runs the same transform
    internally - listing it in babel.config.js made it run twice.

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,6 @@
 module.exports = {
   plugins: [
     "relay",
-    [
-      "@stylexjs/babel-plugin",
-      {
-        dev: false,
-        test: false,
-        runtimeInjection: false,
-        treeshakeCompensation: true,
-        unstable_moduleResolution: {
-          type: "commonJS",
-        },
-      },
-    ],
   ],
   presets: [
     "@babel/preset-env",
